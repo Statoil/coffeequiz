21fc81c2122c52ba75c279e7a985c3485f1e3db6
oyron <oyron@statoil.com>
2017-11-23 09:53:32 +0100

    Improved styling of answer page

--- src/pages/answer/answer.ts
+++ src/pages/answer/answer.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
 import { ViewController } from 'ionic-angular';
+import { Alternative } from "../../app/alternative";
 
 @Component({
   selector: 'page-answer',
@@ -8,10 +9,11 @@ import { ViewController } from 'ionic-angular';
 })
 export class AnswerPage {
 
-  answer: string;
-  truth: string;
+  answer: Alternative;
+  truth: Alternative;
   isCorrect: boolean;
   seconds: number = 3;
+  messageHtml: string;
 
   countDown: number = setInterval(() => {
     this.seconds--;
@@ -24,7 +26,13 @@ export class AnswerPage {
   constructor(public navParams: NavParams, private viewCtrl: ViewController) {
     this.answer = navParams.get('answer');
     this.truth = navParams.get('truth');
-    this.isCorrect = this.answer === this.truth;
+    this.isCorrect = this.answer == this.truth;
+    if (this.isCorrect) {
+      this.messageHtml = `${this.answer.text} is correct!`;
+    }
+    else {
+      this.messageHtml = `Unfortunately,<br><strong>${this.answer.text}</strong><br>is not correct.<br><br>The correct answer is<br><u>${this.truth.text}</u>`;
+    }
   }
 
 }
