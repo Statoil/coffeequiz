09fb455b578be13acea4dfcea7b411133794b606
oyron <oyron@statoil.com>
2019-07-11 08:00:55 +0200

    2.2.1 version

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@
 export const ENV = {
     mode: "dev",
     //apiUrl: 'http://10.53.11.180:3000'
-    apiUrl: 'http://192.168.1.7:3000'
-    //apiUrl: 'https://coffeequiz-test.azurewebsites.net'
+    //apiUrl: 'http://192.168.1.7:3000'
+    apiUrl: 'https://coffeequiz.azurewebsites.net'
     //apiUrl: 'http://localhost:3000'
 };
